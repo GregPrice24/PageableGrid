1403b75f0113bb2ea7c1e9d2210d683cd27806fa
GregPrice24 <Gprice2432@gmail.com>
2017-11-23 17:58:03 -0600

    Initial Commit

---
+++ PageableGrid/PageableGrid.h
@@ -0,0 +1,19 @@
+//
+//  PageableGrid.h
+//  PageableGrid
+//
+//  Created by Gregory Price on 11/23/17.
+//  Copyright © 2017 Gregory Price. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PageableGrid.
+FOUNDATION_EXPORT double PageableGridVersionNumber;
+
+//! Project version string for PageableGrid.
+FOUNDATION_EXPORT const unsigned char PageableGridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PageableGrid/PublicHeader.h>
+
+
